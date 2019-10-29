c7847578d5593e546bc48e8a4e32431d95c6d5d1
Dan Lawrence <dan@myrespace.com>
2019-10-29 11:42:01 +0000

    - Fixed documentation version number.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,9 +31,9 @@ copyright = '2019, Dan Lawrence'
 author = 'Dan Lawrence'
 
 # The short X.Y version
-version = '0.0.1'
+version = '0.2.1'
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.2.1'
 
 
 # -- General configuration ---------------------------------------------------
