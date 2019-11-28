7c5a5651b5db16641c364b8308cb98382b634627
MyreMylar <dan@myrespace.com>
2019-11-28 09:08:44 +0000

    - Adding test to try and kick off travis build.

--- tests/test_ui_message_window.py
+++ tests/test_ui_message_window.py
@@ -14,3 +14,16 @@ class TestUIMessageWindow:
                         message_title="Test Message",
                         html_message="This is a <b>bold</b> test of the message box functionality.",
                         manager=default_ui_manager)
+
+    def test_update(self, _init_pygame, default_ui_manager):
+        message_window = UIMessageWindow(message_window_rect=pygame.Rect(100, 100, 200, 300),
+                                         message_title="Test Message",
+                                         html_message="This is a bold test of the message box functionality.",
+                                         manager=default_ui_manager)
+
+        message_window.close_window_button.pressed = True
+        is_alive_pre_update = message_window.alive()
+        message_window.update(0.01)
+        is_dead_post_update = not message_window.alive()
+
+        assert is_alive_pre_update is True and is_dead_post_update is True
