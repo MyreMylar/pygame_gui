bb67dc61e4665848ee8fb0e509a8819bccc01b1d
MyreMylar <dan@myrespace.com>
2019-11-28 11:10:07 +0000

    - changed test images after raspberry pi testing, no noticeable visual   difference, just three pixels show up as different.

--- tests/test_ui_message_window.py
+++ tests/test_ui_message_window.py
@@ -15,7 +15,7 @@ class TestUIMessageWindow:
                         html_message="This is a <b>bold</b> test of the message box functionality.",
                         manager=default_ui_manager)
 
-    def test_update(self, _init_pygame, default_ui_manager):
+    def test_update_close_button(self, _init_pygame, default_ui_manager):
         message_window = UIMessageWindow(message_window_rect=pygame.Rect(100, 100, 200, 300),
                                          message_title="Test Message",
                                          html_message="This is a bold test of the message box functionality.",
@@ -27,3 +27,27 @@ class TestUIMessageWindow:
         is_dead_post_update = not message_window.alive()
 
         assert is_alive_pre_update is True and is_dead_post_update is True
+
+    def test_update_dismiss_button(self, _init_pygame, default_ui_manager):
+        message_window = UIMessageWindow(message_window_rect=pygame.Rect(100, 100, 200, 300),
+                                         message_title="Test Message",
+                                         html_message="This is a bold test of the message box functionality.",
+                                         manager=default_ui_manager)
+
+        message_window.dismiss_button.pressed = True
+        is_alive_pre_update = message_window.alive()
+        message_window.update(0.01)
+        is_dead_post_update = not message_window.alive()
+
+        assert is_alive_pre_update is True and is_dead_post_update is True
+
+    def test_update_menu_bar_grab(self, _init_pygame, default_ui_manager):
+        message_window = UIMessageWindow(message_window_rect=pygame.Rect(100, 100, 200, 300),
+                                         message_title="Test Message",
+                                         html_message="This is a bold test of the message box functionality.",
+                                         manager=default_ui_manager)
+
+        message_window.menu_bar.held = True
+        message_window.update(0.01)
+
+        assert message_window.grabbed_window is True
