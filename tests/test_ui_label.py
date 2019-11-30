03dfa5fa7169c7a24e555fdeb070d6d1ad39c728
MyreMylar <dan@myrespace.com>
2019-11-30 08:46:04 +0000

    - 2nd batch of tests for UILabel for better coverage

--- tests/test_ui_label.py
+++ tests/test_ui_label.py
@@ -45,11 +45,19 @@ class TestUILabel:
                         manager=manager)
         assert label.image is not None
 
+    def test_rebuild_from_theme_data_non_default_3(self, _init_pygame):
+        manager = UIManager((800, 600), os.path.join("tests", "data", "themes", "ui_label_non_default_3.json"))
+        label = UILabel(relative_rect=pygame.Rect(100, 100, 150, 30),
+                        text="Test Label",
+                        manager=manager)
+        assert label.image is not None
+
     @pytest.mark.filterwarnings("ignore:Invalid value")
     @pytest.mark.filterwarnings("ignore:Colour hex code")
+    @pytest.mark.filterwarnings("ignore:Label Rect is too small for text")
     def test_rebuild_from_theme_data_bad_values(self, _init_pygame):
         manager = UIManager((800, 600), os.path.join("tests", "data", "themes", "ui_label_bad_values.json"))
-        label = UILabel(relative_rect=pygame.Rect(100, 100, 150, 30),
+        label = UILabel(relative_rect=pygame.Rect(100, 100, 10, 30),
                         text="Test Label",
                         manager=manager)
         assert label.image is not None
