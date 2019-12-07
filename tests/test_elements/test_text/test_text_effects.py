acf178c31704789a173e2e89630768608a8a4113
Dan Lawrence <dan@myrespace.com>
2019-12-07 21:18:21 +0000

    - Added Text Effect tests

---
+++ tests/test_elements/test_text/test_text_effects.py
@@ -0,0 +1,28 @@
+import os
+import pytest
+import pygame
+import pygame_gui
+
+from tests.shared_fixtures import _init_pygame, default_ui_manager, default_display_surface, _display_surface_return_none
+
+from pygame_gui.elements.text.text_effects import TextBoxEffect, TypingAppearEffect, FadeInEffect, FadeOutEffect
+
+
+class TestTextEffects:
+    def test_creation(self, _init_pygame):
+        text_effect = TextBoxEffect('test')
+        text_effect.update(0.5)
+        assert text_effect.get_end_text_pos() == 4
+        assert text_effect.get_final_alpha() == 255
+
+    def test_create_typing_effect(self, _init_pygame):
+        text_effect = TypingAppearEffect('test')
+        assert text_effect.should_full_redraw() is False
+
+    def test_create_fade_in_effect(self, _init_pygame):
+        text_effect = FadeInEffect('test')
+        assert text_effect.should_full_redraw() is False
+
+    def test_create_fade_out_effect(self, _init_pygame):
+        text_effect = FadeOutEffect('test')
+        assert text_effect.should_full_redraw() is False
