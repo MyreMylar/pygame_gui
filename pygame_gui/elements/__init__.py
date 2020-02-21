d70dae9ccb562b71c7c357769c8c3f2e908dbd37
MyreMylar <dan@myrespace.com>
2020-02-21 15:04:26 +0000

    - Added UIPanel to __init__.py.

--- pygame_gui/elements/__init__.py
+++ pygame_gui/elements/__init__.py
@@ -3,6 +3,7 @@ from pygame_gui.elements.ui_button import UIButton
 from pygame_gui.elements.ui_horizontal_slider import UIHorizontalSlider
 from pygame_gui.elements.ui_vertical_scroll_bar import UIVerticalScrollBar
 from pygame_gui.elements.ui_label import UILabel
+from pygame_gui.elements.ui_panel import UIPanel
 from pygame_gui.elements.ui_screen_space_health_bar import UIScreenSpaceHealthBar
 from pygame_gui.elements.ui_tool_tip import UITooltip
 from pygame_gui.elements.ui_drop_down_menu import UIDropDownMenu
