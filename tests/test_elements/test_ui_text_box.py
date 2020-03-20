6d302871ed094b9d3e491d03437dd0d64a9051ba
MyreMylar <dan@myrespace.com>
2020-03-20 13:07:44 +0000

    - Added test for issue #63, short text boxes not auto expanding to height when parameter 'wrap_to_height' set.

--- tests/test_elements/test_ui_text_box.py
+++ tests/test_elements/test_ui_text_box.py
@@ -3,7 +3,8 @@ import pytest
 import pygame
 import pygame_gui
 
-from tests.shared_fixtures import _init_pygame, default_ui_manager, default_display_surface, _display_surface_return_none
+from tests.shared_fixtures import _init_pygame, default_ui_manager, default_display_surface, \
+    _display_surface_return_none
 
 from pygame_gui.ui_manager import UIManager
 from pygame_gui.elements.ui_text_box import UITextBox
@@ -43,40 +44,40 @@ class TestUITextBox:
                                           {'name': 'fira_code', 'html_size': 2, 'style': 'bold'},
                                           {'name': 'fira_code', 'html_size': 2, 'style': 'bold_italic'}])
         text_box = UITextBox(html_text=''
-                             '<font color=regular_text><font color=#E784A2 size=4.5><br><b><u>Lorem</u><br><br><br>'
-                             'ipsum dolor sit amet</b></font>,'
-                             ' <b><a href="test">consectetur</a></b> adipiscing elit. in a flibb de dib do '
-                             'rub a la clob slip the perry tin fo glorp yip dorp'
-                             'skorp si pork flum de dum be dung, slob be robble glurp destination flum kin slum. '
-                             'Ram slim gordo, fem tulip squirrel slippers save socks certainly.<br>'
-                             'Vestibulum in <i>commodo me</i> tellus in nisi finibus a sodales.<br>Vestibulum'
-                             '<font size=2>hendrerit mi <i>sed nulla</i> scelerisque</font>, posuere ullamcorper '
-                             'sem pulvinar.'
-                             'Nulla at pulvinar a odio, a dictum dolor.<br>Maecenas at <font size=6><b>tellus a'
-                             'tortor. a<br>'
-                             'In <i>bibendum</i> orci et velit</b> gravida lacinia.<br><br>'
-                             'In hac a habitasse to platea dictumst.<br>'
-                             '<font color=#4CD656 size=4>Vivamus I interdum mollis lacus nec porttitor.<br>Morbi '
-                             'accumsan, lectus at '
-                             'tincidunt to dictum, neque <font color=#879AF6>erat tristique erat</font>, '
-                             'sed a tempus for <b>nunc</b> dolor in nibh.<br>'
-                             'Suspendisse in viverra dui <i>fringilla dolor laoreet</i>, sit amet on pharetra a ante '
-                             'sollicitudin.</font></font>'
-                             '<br><br>'
-                             '<b>consectetur</b> adipiscing elit. in a<br>'
-                             'Vestibulum in <i>commodo me</i> tellus in nisi finibus a sodales.<br>'
-                             'Vestibulum <font size=2>hendrerit mi <i>sed nulla</i> scelerisque</font>, '
-                             'posuere ullamcorper sem pulvinar. '
-                             'Nulla at pulvinar a odio, a dictum dolor.<br>'
-                             'Maecenas at <font size=6><b>tellus a tortor. a<br>'
-                             'In <i>bibendum</i> orci et velit</b> gravida lacinia.<br><br>'
-                             'In hac a habitasse to platea dictumst.<br>'
-                             '<font color=#4CD656 size=4>Vivamus I interdum mollis lacus nec porttitor.<br>Morbi '
-                             'accumsan, lectus at'
-                             'tincidunt to dictum, neque <font color=#879AF6>erat tristique erat</font>, '
-                             'sed a tempus for <b>nunc</b> dolor in nibh.<br>'
-                             'Suspendisse in viverra dui <i>fringilla dolor laoreet</i>, sit amet on pharetra a ante '
-                             'sollicitudin.</font></font>',
+                                       '<font color=regular_text><font color=#E784A2 size=4.5><br><b><u>Lorem</u><br><br><br>'
+                                       'ipsum dolor sit amet</b></font>,'
+                                       ' <b><a href="test">consectetur</a></b> adipiscing elit. in a flibb de dib do '
+                                       'rub a la clob slip the perry tin fo glorp yip dorp'
+                                       'skorp si pork flum de dum be dung, slob be robble glurp destination flum kin slum. '
+                                       'Ram slim gordo, fem tulip squirrel slippers save socks certainly.<br>'
+                                       'Vestibulum in <i>commodo me</i> tellus in nisi finibus a sodales.<br>Vestibulum'
+                                       '<font size=2>hendrerit mi <i>sed nulla</i> scelerisque</font>, posuere ullamcorper '
+                                       'sem pulvinar.'
+                                       'Nulla at pulvinar a odio, a dictum dolor.<br>Maecenas at <font size=6><b>tellus a'
+                                       'tortor. a<br>'
+                                       'In <i>bibendum</i> orci et velit</b> gravida lacinia.<br><br>'
+                                       'In hac a habitasse to platea dictumst.<br>'
+                                       '<font color=#4CD656 size=4>Vivamus I interdum mollis lacus nec porttitor.<br>Morbi '
+                                       'accumsan, lectus at '
+                                       'tincidunt to dictum, neque <font color=#879AF6>erat tristique erat</font>, '
+                                       'sed a tempus for <b>nunc</b> dolor in nibh.<br>'
+                                       'Suspendisse in viverra dui <i>fringilla dolor laoreet</i>, sit amet on pharetra a ante '
+                                       'sollicitudin.</font></font>'
+                                       '<br><br>'
+                                       '<b>consectetur</b> adipiscing elit. in a<br>'
+                                       'Vestibulum in <i>commodo me</i> tellus in nisi finibus a sodales.<br>'
+                                       'Vestibulum <font size=2>hendrerit mi <i>sed nulla</i> scelerisque</font>, '
+                                       'posuere ullamcorper sem pulvinar. '
+                                       'Nulla at pulvinar a odio, a dictum dolor.<br>'
+                                       'Maecenas at <font size=6><b>tellus a tortor. a<br>'
+                                       'In <i>bibendum</i> orci et velit</b> gravida lacinia.<br><br>'
+                                       'In hac a habitasse to platea dictumst.<br>'
+                                       '<font color=#4CD656 size=4>Vivamus I interdum mollis lacus nec porttitor.<br>Morbi '
+                                       'accumsan, lectus at'
+                                       'tincidunt to dictum, neque <font color=#879AF6>erat tristique erat</font>, '
+                                       'sed a tempus for <b>nunc</b> dolor in nibh.<br>'
+                                       'Suspendisse in viverra dui <i>fringilla dolor laoreet</i>, sit amet on pharetra a ante '
+                                       'sollicitudin.</font></font>',
                              relative_rect=pygame.Rect(100, 100, 200, 300),
                              manager=default_ui_manager)
 
@@ -111,40 +112,40 @@ class TestUITextBox:
                                           {'name': 'fira_code', 'html_size': 2, 'style': 'bold'},
                                           {'name': 'fira_code', 'html_size': 2, 'style': 'bold_italic'}])
         text_box = UITextBox(html_text=''
-                             '<font color=regular_text><font color=#E784A2 size=4.5><br><b><u>Lorem</u><br><br><br>'
-                             'ipsum dolor sit amet</b></font>,'
-                             ' <b><a href="test">consectetur</a></b> adipiscing elit. in a flibb de dib do '
-                             'rub a la clob slip the perry tin fo glorp yip dorp'
-                             'skorp si pork flum de dum be dung, slob be robble glurp destination flum kin slum. '
-                             'Ram slim gordo, fem tulip squirrel slippers save socks certainly.<br>'
-                             'Vestibulum in <i>commodo me</i> tellus in nisi finibus a sodales.<br>Vestibulum'
-                             '<font size=2>hendrerit mi <i>sed nulla</i> scelerisque</font>, posuere ullamcorper '
-                             'sem pulvinar.'
-                             'Nulla at pulvinar a odio, a dictum dolor.<br>Maecenas at <font size=6><b>tellus a'
-                             'tortor. a<br>'
-                             'In <i>bibendum</i> orci et velit</b> gravida lacinia.<br><br>'
-                             'In hac a habitasse to platea dictumst.<br>'
-                             '<font color=#4CD656 size=4>Vivamus I interdum mollis lacus nec porttitor.<br>Morbi '
-                             'accumsan, lectus at '
-                             'tincidunt to dictum, neque <font color=#879AF6>erat tristique erat</font>, '
-                             'sed a tempus for <b>nunc</b> dolor in nibh.<br>'
-                             'Suspendisse in viverra dui <i>fringilla dolor laoreet</i>, sit amet on pharetra a ante '
-                             'sollicitudin.</font></font>'
-                             '<br><br>'
-                             '<b>consectetur</b> adipiscing elit. in a<br>'
-                             'Vestibulum in <i>commodo me</i> tellus in nisi finibus a sodales.<br>'
-                             'Vestibulum <font size=2>hendrerit mi <i>sed nulla</i> scelerisque</font>, '
-                             'posuere ullamcorper sem pulvinar. '
-                             'Nulla at pulvinar a odio, a dictum dolor.<br>'
-                             'Maecenas at <font size=6><b>tellus a tortor. a<br>'
-                             'In <i>bibendum</i> orci et velit</b> gravida lacinia.<br><br>'
-                             'In hac a habitasse to platea dictumst.<br>'
-                             '<font color=#4CD656 size=4>Vivamus I interdum mollis lacus nec porttitor.<br>Morbi '
-                             'accumsan, lectus at'
-                             'tincidunt to dictum, neque <font color=#879AF6>erat tristique erat</font>, '
-                             'sed a tempus for <b>nunc</b> dolor in nibh.<br>'
-                             'Suspendisse in viverra dui <i>fringilla dolor laoreet</i>, sit amet on pharetra a ante '
-                             'sollicitudin.</font></font>',
+                                       '<font color=regular_text><font color=#E784A2 size=4.5><br><b><u>Lorem</u><br><br><br>'
+                                       'ipsum dolor sit amet</b></font>,'
+                                       ' <b><a href="test">consectetur</a></b> adipiscing elit. in a flibb de dib do '
+                                       'rub a la clob slip the perry tin fo glorp yip dorp'
+                                       'skorp si pork flum de dum be dung, slob be robble glurp destination flum kin slum. '
+                                       'Ram slim gordo, fem tulip squirrel slippers save socks certainly.<br>'
+                                       'Vestibulum in <i>commodo me</i> tellus in nisi finibus a sodales.<br>Vestibulum'
+                                       '<font size=2>hendrerit mi <i>sed nulla</i> scelerisque</font>, posuere ullamcorper '
+                                       'sem pulvinar.'
+                                       'Nulla at pulvinar a odio, a dictum dolor.<br>Maecenas at <font size=6><b>tellus a'
+                                       'tortor. a<br>'
+                                       'In <i>bibendum</i> orci et velit</b> gravida lacinia.<br><br>'
+                                       'In hac a habitasse to platea dictumst.<br>'
+                                       '<font color=#4CD656 size=4>Vivamus I interdum mollis lacus nec porttitor.<br>Morbi '
+                                       'accumsan, lectus at '
+                                       'tincidunt to dictum, neque <font color=#879AF6>erat tristique erat</font>, '
+                                       'sed a tempus for <b>nunc</b> dolor in nibh.<br>'
+                                       'Suspendisse in viverra dui <i>fringilla dolor laoreet</i>, sit amet on pharetra a ante '
+                                       'sollicitudin.</font></font>'
+                                       '<br><br>'
+                                       '<b>consectetur</b> adipiscing elit. in a<br>'
+                                       'Vestibulum in <i>commodo me</i> tellus in nisi finibus a sodales.<br>'
+                                       'Vestibulum <font size=2>hendrerit mi <i>sed nulla</i> scelerisque</font>, '
+                                       'posuere ullamcorper sem pulvinar. '
+                                       'Nulla at pulvinar a odio, a dictum dolor.<br>'
+                                       'Maecenas at <font size=6><b>tellus a tortor. a<br>'
+                                       'In <i>bibendum</i> orci et velit</b> gravida lacinia.<br><br>'
+                                       'In hac a habitasse to platea dictumst.<br>'
+                                       '<font color=#4CD656 size=4>Vivamus I interdum mollis lacus nec porttitor.<br>Morbi '
+                                       'accumsan, lectus at'
+                                       'tincidunt to dictum, neque <font color=#879AF6>erat tristique erat</font>, '
+                                       'sed a tempus for <b>nunc</b> dolor in nibh.<br>'
+                                       'Suspendisse in viverra dui <i>fringilla dolor laoreet</i>, sit amet on pharetra a ante '
+                                       'sollicitudin.</font></font>',
                              relative_rect=pygame.Rect(100, 100, 200, 300),
                              manager=default_ui_manager)
 
@@ -164,12 +165,12 @@ class TestUITextBox:
 
     def test_on_fresh_drawable_shape_ready(self, _init_pygame: None, default_ui_manager: UIManager):
         text_box = UITextBox(html_text='la la LA LA LAL LAL ALALA'
-                                      'LLALAALALA ALALA ALAL ALA'
-                                      'LAALA ALALA ALALA AAaal aa'
-                                      'ALALAa laalal alalal alala'
-                                      'alalalala alalalalalal alal'
-                                      'alalalala alala alalala ala'
-                                      'alalalalal lalal alalalal al',
+                                       'LLALAALALA ALALA ALAL ALA'
+                                       'LAALA ALALA ALALA AAaal aa'
+                                       'ALALAa laalal alalal alala'
+                                       'alalalala alalalalalal alal'
+                                       'alalalala alala alalala ala'
+                                       'alalalalal lalal alalalal al',
                              relative_rect=pygame.Rect(100, 100, 100, 100),
                              manager=default_ui_manager)
         text_box.on_fresh_drawable_shape_ready()
@@ -481,3 +482,38 @@ class TestUITextBox:
         default_ui_manager.process_events(pygame.event.Event(pygame.MOUSEBUTTONDOWN, {'button': 1, 'pos': (195, 75)}))
         # if we successfully clicked on the moved slider then this button should be True
         assert text_box.scroll_bar.bottom_button.held is True
+
+    def test_create_very_short_text_box(self, _init_pygame, default_ui_manager):
+        x = 640
+        y = 360
+        width = 320
+        height = 10  # height will auto increase
+        text = "this is a test"
+
+        rect = pygame.Rect((x, y), (width, height))
+
+        text_box = UITextBox(html_text=text,
+                             relative_rect=rect,
+                             manager=default_ui_manager,
+                             wrap_to_height=True,
+                             layer_starting_height=100,
+                             object_id="screen_message")
+
+        assert text_box.rect.height != 10
+
+        x = 640
+        y = 360
+        width = 320
+        height = 0  # height will auto increase
+        text = "this is a test"
+
+        rect = pygame.Rect((x, y), (width, height))
+
+        text_box = UITextBox(html_text=text,
+                             relative_rect=rect,
+                             manager=default_ui_manager,
+                             wrap_to_height=True,
+                             layer_starting_height=100,
+                             object_id="screen_message")
+
+        assert text_box.rect.height != 0
