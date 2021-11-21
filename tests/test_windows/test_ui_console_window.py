994907ab893f5fd40f511bfe28f07262fcd48899
MyreMylar <dan@myrespace.com>
2021-11-21 17:36:18 +0000

    fix console window tests

--- tests/test_windows/test_ui_console_window.py
+++ tests/test_windows/test_ui_console_window.py
@@ -50,7 +50,7 @@ class TestUIConsoleWindow:
 
         logged_line = console_window.log_prefix + '&lt;b&gt;A line of text to log&lt;/b&gt;<br>'
 
-        assert console_window.log.html_text == logged_line
+        assert console_window.log.appended_text == logged_line
         assert console_window.command_entry.get_text() == ''
 
         console_window.set_logged_commands_escape_html(should_escape=False)
@@ -74,7 +74,7 @@ class TestUIConsoleWindow:
         logged_lines = (console_window.log_prefix + '&lt;b&gt;A line of text to log&lt;/b&gt;<br>' +
                         console_window.log_prefix + '<b>A second line of text to log</b><br>')
 
-        assert console_window.log.html_text == logged_lines
+        assert console_window.log.appended_text == logged_lines
         assert console_window.command_entry.get_text() == ''
 
     def test_log_prefix(self, _init_pygame, default_ui_manager,
@@ -102,7 +102,7 @@ class TestUIConsoleWindow:
 
         logged_line = '++ A line of text to log<br>'
 
-        assert console_window.log.html_text == logged_line
+        assert console_window.log.appended_text == logged_line
 
         console_window.set_log_prefix('-- ')
 
@@ -125,7 +125,7 @@ class TestUIConsoleWindow:
 
         logged_lines = '++ A line of text to log<br>-- A second line of text to log<br>'
 
-        assert console_window.log.html_text == logged_lines
+        assert console_window.log.appended_text == logged_lines
 
         console_window.restore_default_prefix()
 
@@ -150,7 +150,7 @@ class TestUIConsoleWindow:
                         '-- A second line of text to log<br>'
                         '> A third line of text to log<br>')
 
-        assert console_window.log.html_text == logged_lines
+        assert console_window.log.appended_text == logged_lines
 
     def test_add_output_line_to_log(self, _init_pygame, default_ui_manager,
                                     _display_surface_return_none):
@@ -159,22 +159,22 @@ class TestUIConsoleWindow:
 
         console_window.add_output_line_to_log('line to add')
 
-        assert console_window.log.html_text == '<b>line to add</b><br>'
+        assert console_window.log.appended_text == '<b>line to add</b><br>'
 
         console_window.add_output_line_to_log('second line to add, no line break',
                                               is_bold=False,
                                               remove_line_break=True)
 
-        assert console_window.log.html_text == ('<b>line to add</b><br>'
-                                                'second line to add, no line break')
+        assert console_window.log.appended_text == ('<b>line to add</b><br>'
+                                                    'second line to add, no line break')
 
         console_window.add_output_line_to_log('third <i>line</i> to add, escape html',
                                               escape_html=True)
 
-        assert console_window.log.html_text == ('<b>line to add</b><br>'
-                                                'second line to add, no line break'
-                                                '<b>third &lt;i&gt;line&lt;/i&gt; to add,'
-                                                ' escape html</b><br>')
+        assert console_window.log.appended_text == ('<b>line to add</b><br>'
+                                                    'second line to add, no line break'
+                                                    '<b>third &lt;i&gt;line&lt;/i&gt; to add,'
+                                                    ' escape html</b><br>')
 
     def test_scroll_log_with_arrow_keys(self, _init_pygame, default_ui_manager,
                                         _display_surface_return_none):
