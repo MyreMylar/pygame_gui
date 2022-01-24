6d39856048fdeb30d29f7fdd7869c8a64975c269
rjh <ron@conquerprogramming.com>
2022-01-24 10:52:00 -0800

    Refactored UIScreenSpaceHealthBar to be subclass of UIStatusBar.

--- pygame_gui/elements/ui_screen_space_health_bar.py
+++ pygame_gui/elements/ui_screen_space_health_bar.py
@@ -5,10 +5,10 @@ import pygame
 from pygame_gui.core import ObjectID
 from pygame_gui.core.interfaces import IContainerLikeInterface, IUIManagerInterface
 from pygame_gui.core import UIElement
-from pygame_gui.core.drawable_shapes import RectDrawableShape, RoundedRectangleShape
+from pygame_gui.elements.ui_status_bar import UIStatusBar
 
 
-class UIScreenSpaceHealthBar(UIElement):
+class UIScreenSpaceHealthBar(UIStatusBar):
     """
     A UI that will display health capacity and current health for a sprite in 'screen space'.
     That means it won't move with the camera. This is a good choice for a user/player sprite.
@@ -24,6 +24,8 @@ class UIScreenSpaceHealthBar(UIElement):
     :param visible: Whether the element is visible by default. Warning - container visibility
                     may override this.
     """
+    element_id = 'screen_space_health_bar'
+
     def __init__(self,
                  relative_rect: pygame.Rect,
                  manager: IUIManagerInterface,
@@ -34,198 +36,108 @@ class UIScreenSpaceHealthBar(UIElement):
                  anchors: Dict[str, str] = None,
                  visible: int = 1):
 
-        super().__init__(relative_rect, manager, container,
-                         starting_height=1,
-                         layer_thickness=1,
-                         anchors=anchors,
-                         visible=visible)
-
-        self._create_valid_ids(container=container,
-                               parent_element=parent_element,
-                               object_id=object_id,
-                               element_id='screen_space_health_bar')
-
-        self.current_health = 50
-        self.health_capacity = 100
-        self.health_percentage = self.current_health / self.health_capacity
-
         self.font = None
-        self.border_width = None
-        self.shadow_width = None
-        self.border_colour = None
-        self.bar_unfilled_colour = None
-        self.bar_filled_colour = None
         self.text_shadow_colour = None
         self.text_colour = None
         self.text_horiz_alignment = 'center'
         self.text_vert_alignment = 'center'
         self.text_horiz_alignment_padding = 1
         self.text_vert_alignment_padding = 1
-
-        self.border_rect = None
-        self.capacity_width = None
-        self.capacity_height = None
-        self.capacity_rect = None
-        self.current_health_rect = None
-
-        self.drawable_shape = None
-        self.shape = 'rectangle'
-        self.shape_corner_radius = None
-
-        if sprite_to_monitor is not None:
-            if not hasattr(sprite_to_monitor, 'health_capacity'):
-                raise AttributeError('Sprite does not have health_capacity attribute')
-            if not hasattr(sprite_to_monitor, 'current_health'):
-                raise AttributeError('Sprite does not have current_health attribute')
-            self.sprite_to_monitor = sprite_to_monitor
-        else:
-            self.sprite_to_monitor = None
-        self.set_image(None)
         self.background_text = None
         self.foreground_text = None
+        self._sprite_to_monitor = None
 
-        self.rebuild_from_changed_theme_data()
-
-    def set_sprite_to_monitor(self, sprite_to_monitor: pygame.sprite.Sprite):
-        """
-        Sprite to monitor the health of. Must have 'health_capacity' and 'current_health'
-        attributes.
-
-        :param sprite_to_monitor:
-
-        """
-        if not hasattr(sprite_to_monitor, 'health_capacity'):
-            raise AttributeError('Sprite does not have health_capacity attribute')
-        if not hasattr(sprite_to_monitor, 'current_health'):
-            raise AttributeError('Sprite does not have current_health attribute')
+        # Set this using the property, for error handling.
         self.sprite_to_monitor = sprite_to_monitor
 
-    def rebuild(self):
-        """
-        Rebuild the health bar entirely because the theming data has changed.
+        super().__init__(relative_rect=relative_rect,
+                         manager=manager,
+                         display_sprite=None,
+                         percent_method=self.health_percent,
+                         container=container,
+                         parent_element=parent_element,
+                         object_id=object_id,
+                         anchors=anchors,
+                         visible=visible)
 
-        """
-        border_rect_width = self.rect.width - (self.shadow_width * 2)
-        border_rect_height = self.rect.height - (self.shadow_width * 2)
-        self.border_rect = pygame.Rect((self.shadow_width,
-                                        self.shadow_width),
-                                       (border_rect_width, border_rect_height))
+    @property
+    def current_health(self):
+        return self.sprite_to_monitor.current_health
 
-        self.capacity_width = self.rect.width - (self.shadow_width * 2) - self.border_width * 2
-        self.capacity_height = self.rect.height - (self.shadow_width * 2) - self.border_width * 2
-        self.capacity_rect = pygame.Rect((self.shadow_width + self.border_width,
-                                          self.shadow_width + self.border_width),
-                                         (self.capacity_width, self.capacity_height))
+    @property
+    def health_capacity(self):
+        return self.sprite_to_monitor.health_capacity
 
-        self.current_health_rect = pygame.Rect((self.shadow_width + self.border_width,
-                                                self.shadow_width + self.border_width),
-                                               (int(self.capacity_width * self.health_percentage),
-                                                self.capacity_height))
+    def health_percent(self):
+        return self.current_health / self.health_capacity
 
-        self.redraw()
+    @property
+    def health_percentage(self):
+        # Now that we subclass UIStatusBar, this is here for backward compatibility.
+        return self.health_percent()
 
-    def redraw(self):
+    @property
+    def sprite_to_monitor(self):
+        """ Sprite to monitor the health of. Must have 'health_capacity' and 'current_health'
+        attributes. """
+        return self._sprite_to_monitor
+
+    @sprite_to_monitor.setter
+    def sprite_to_monitor(self, sprite: pygame.sprite.Sprite):
+        if sprite:
+            if not hasattr(sprite, 'health_capacity'):
+                raise AttributeError('Sprite does not have health_capacity attribute')
+            if not hasattr(sprite, 'current_health'):
+                raise AttributeError('Sprite does not have current_health attribute')
+        self._sprite_to_monitor = sprite
+
+    def set_sprite_to_monitor(self, sprite_to_monitor: pygame.sprite.Sprite):
+        # This method is here for backward compatibility.
+        self.sprite_to_monitor = sprite_to_monitor
+
+    @property
+    def health_display_string(self):
+        """ Subclass and override this property to change what text is displayed, or to suppress the text. """
+        return f"{self.current_health:0.1f}/{self.health_capacity:0.1f}"
+
+    def redraw(self, theming_parameters=None):
         """
         Redraws the health bar rectangles and text onto the underlying sprite's image surface.
         Takes a little while so we only do it when the health has changed.
         """
-        health_display_string = str(self.current_health) + "/" + str(self.health_capacity)
+        if theming_parameters is None:
+            theming_parameters = {}
 
-        theming_parameters = {'normal_bg': self.bar_unfilled_colour,
-                              'normal_border': self.border_colour,
-                              'border_width': self.border_width,
-                              'shadow_width': self.shadow_width,
-                              'shape_corner_radius': self.shape_corner_radius,
-                              'filled_bar': self.bar_filled_colour,
-                              'filled_bar_width_percentage': self.health_percentage,
-                              'font': self.font,
-                              'text': health_display_string,
-                              'normal_text': self.text_colour,
-                              'normal_text_shadow': self.text_shadow_colour,
-                              'text_shadow': (1,
-                                              0,
-                                              0,
-                                              self.text_shadow_colour,
-                                              False),
-                              'text_horiz_alignment': self.text_horiz_alignment,
-                              'text_vert_alignment': self.text_vert_alignment,
-                              'text_horiz_alignment_padding': self.text_horiz_alignment_padding,
-                              'text_vert_alignment_padding': self.text_vert_alignment_padding,
-                              }
+        parameters = {'font': self.font,
+                      'text': self.health_display_string,
+                      'normal_text': self.text_colour,
+                      'normal_text_shadow': self.text_shadow_colour,
+                      'text_shadow': (1,
+                                      0,
+                                      0,
+                                      self.text_shadow_colour,
+                                      False),
+                      'text_horiz_alignment': self.text_horiz_alignment,
+                      'text_vert_alignment': self.text_vert_alignment,
+                      'text_horiz_alignment_padding': self.text_horiz_alignment_padding,
+                      'text_vert_alignment_padding': self.text_vert_alignment_padding,
+                      }
 
-        if self.shape == 'rectangle':
-            self.drawable_shape = RectDrawableShape(self.rect, theming_parameters,
-                                                    ['normal'], self.ui_manager)
-        elif self.shape == 'rounded_rectangle':
-            self.drawable_shape = RoundedRectangleShape(self.rect, theming_parameters,
-                                                        ['normal'], self.ui_manager)
+        # If there are duplicate entries, let the subclass's values overwrite mine.
+        parameters.update(theming_parameters)
+        super().redraw(parameters)
 
-        self.set_image(self.drawable_shape.get_fresh_surface())
-
-    def update(self, time_delta: float):
-        """
-        Updates the health bar sprite's image with the latest health data from the
-        sprite we are monitoring. Only triggers a rebuild if the health values have changed.
-
-        :param time_delta: time passed in seconds between one call to this method and the next.
-
-        """
-        super().update(time_delta)
-        if (self.alive() and self.sprite_to_monitor is not None and
-                (self.sprite_to_monitor.health_capacity != self.health_capacity or
-                 self.current_health != self.sprite_to_monitor.current_health)):
-            self.current_health = self.sprite_to_monitor.current_health
-            self.health_capacity = self.sprite_to_monitor.health_capacity
-            self.health_percentage = self.current_health / self.health_capacity
-
-            rect_width = int(self.capacity_width * self.health_percentage)
-            self.current_health_rect = pygame.Rect((self.shadow_width + self.border_width,
-                                                    self.shadow_width + self.border_width),
-                                                   (rect_width,
-                                                    self.capacity_height))
-            self.redraw()
-
-    def rebuild_from_changed_theme_data(self):
+    def rebuild_from_changed_theme_data(self, has_any_changed=False):
         """
         Called by the UIManager to check the theming data and rebuild whatever needs rebuilding
         for this element when the theme data has changed.
+
+        :param has_any_changed: allows subclasses to do their own theme check and pass this along.
         """
-        super().rebuild_from_changed_theme_data()
-        has_any_changed = False
 
         font = self.ui_theme.get_font(self.combined_element_ids)
         if font != self.font:
             self.font = font
-            has_any_changed = True
-
-        if self._check_misc_theme_data_changed(attribute_name='shape',
-                                               default_value='rectangle',
-                                               casting_func=str,
-                                               allowed_values=['rectangle',
-                                                               'rounded_rectangle']):
-            has_any_changed = True
-
-        if self._check_shape_theming_changed(defaults={'border_width': 1,
-                                                       'shadow_width': 2,
-                                                       'shape_corner_radius': 2}):
-            has_any_changed = True
-
-        border_colour = self.ui_theme.get_colour_or_gradient('normal_border',
-                                                             self.combined_element_ids)
-        if border_colour != self.border_colour:
-            self.border_colour = border_colour
-            has_any_changed = True
-
-        bar_unfilled_colour = self.ui_theme.get_colour_or_gradient('unfilled_bar',
-                                                                   self.combined_element_ids)
-        if bar_unfilled_colour != self.bar_unfilled_colour:
-            self.bar_unfilled_colour = bar_unfilled_colour
-            has_any_changed = True
-
-        bar_filled_colour = self.ui_theme.get_colour_or_gradient('filled_bar',
-                                                                 self.combined_element_ids)
-        if bar_filled_colour != self.bar_filled_colour:
-            self.bar_filled_colour = bar_filled_colour
             has_any_changed = True
 
         text_shadow_colour = self.ui_theme.get_colour('text_shadow', self.combined_element_ids)
@@ -238,5 +150,4 @@ class UIScreenSpaceHealthBar(UIElement):
             self.text_colour = text_colour
             has_any_changed = True
 
-        if has_any_changed:
-            self.rebuild()
+        super().rebuild_from_changed_theme_data(has_any_changed)
