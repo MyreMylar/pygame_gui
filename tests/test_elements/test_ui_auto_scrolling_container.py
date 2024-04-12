8dee0c65472b8558c7842339c95754d5bec1f0f7
Dan Lawrence <danintheshed@gmail.com>
2024-04-12 12:04:13 +0100

    Scrolling container changes for UIForm

--- tests/test_elements/test_ui_auto_scrolling_container.py
+++ tests/test_elements/test_ui_auto_scrolling_container.py
@@ -130,13 +130,15 @@ class TestUIAutoScrollingContainer:
                                              manager=default_ui_manager,
                                              allow_scroll_x=False)
 
-        assert container.vert_scroll_bar is None
+        assert container.vert_scroll_bar is not None
+        assert container.vert_scroll_bar.rect.width == 0
         assert container.horiz_scroll_bar is None
         assert container.scrollable_container.rect.size == (200, 200)
 
         container.set_scrollable_area_dimensions((200, 600))
 
         assert container.vert_scroll_bar is not None
+        assert container.vert_scroll_bar.rect.width != 0
         assert container.horiz_scroll_bar is None
         assert container._view_container.rect.size == (200 - container.vert_scroll_bar.rect.width,
                                                        200)
@@ -149,13 +151,15 @@ class TestUIAutoScrollingContainer:
                                              allow_scroll_y=False)
 
         assert container.vert_scroll_bar is None
-        assert container.horiz_scroll_bar is None
+        assert container.horiz_scroll_bar is not None
+        assert container.horiz_scroll_bar.rect.height == 0
         assert container.scrollable_container.rect.size == (200, 200)
 
         container.set_scrollable_area_dimensions((600, 200))
 
         assert container.vert_scroll_bar is None
         assert container.horiz_scroll_bar is not None
+        assert container.horiz_scroll_bar.rect.height != 0
         assert container._view_container.rect.size == (200,
                                                        200 - container.horiz_scroll_bar.rect.height)
         assert container.scrollable_container.rect.size == (600,
