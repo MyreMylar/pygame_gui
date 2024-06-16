c25d04e515f3ccd89f9eaf1c6c8c09f2ede7deed
Dan Lawrence <danintheshed@gmail.com>
2024-06-16 17:59:57 +0100

    update to version 0.6.12

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,9 +34,9 @@ copyright = '2019, Dan Lawrence'
 author = 'Dan Lawrence'
 
 # The short X.Y version
-version = '0.6.11'
+version = '0.6.12'
 # The full version, including alpha/beta/rc tags
-release = '0.6.11'
+release = '0.6.12'
 
 
 # -- General configuration ---------------------------------------------------
